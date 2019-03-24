aaaf6b075dda3939e5c046857ab4a99cac330349
Ian Cook Westgate <iwestgate@hotmail.com>
2019-03-24 11:36:31 -0700

    initial commit.

---
+++ app/typescript-addition.js
@@ -0,0 +1,7 @@
+var findSum = function (firstValue, secondValue) {
+    var sum = firstValue + secondValue;
+    alert("The sum of your two numbers is: " + sum);
+};
+var firstNumber = prompt('Please enter a number.');
+var secondNumber = prompt('Enter another number.');
+findSum(firstNumber, secondNumber);
